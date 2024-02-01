90e76bb287d2952af6a89bd75f6b6eab7bdcc7c2
mattpe <mattpe@metropolia.fi>
2024-02-01 15:29:27 +0200

    starting router implemantion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Home from "./components/Home";
+import Home from "./views/Home";
+import Profile from "./views/Profile";
+import Single from "./views/Single";
+import Upload from "./views/Upload";
 
 const App = () => {
 
   return (
     <>
       <h1>My app</h1>
+    {/* TODO: Implement browser router for switching between views */}
       <Home />
+      <Profile />
+      <Single />
+      <Upload />
     </>
   );
 };
---
+++ src/views/Single.tsx
@@ -0,0 +1,5 @@
+const Single = () => {
+  return <h2>Single page</h2>
+};
+
+export default Single;
