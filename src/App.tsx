10c954ba8c4478840124b588561f5ab0f3672d38
mattpe <mattpe@metropolia.fi>
2024-02-01 13:42:22 +0200

    some state demos in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import {useState} from "react";
 import Home from "./components/Home";
 
 const App = () => {
+  const [text, setText] = useState('kukkuu');
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
+      <h2 onClick={()=>{
+        setCounter(counter + 1);
+        setText('Uutta asiaa');
+      }}>{text} Counter: {counter}</h2>
       <h1>My app</h1>
       <Home />
     </>
