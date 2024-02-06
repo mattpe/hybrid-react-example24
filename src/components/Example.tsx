626a84594b98a017be71ec949629f55d9256f5be
mattpe <mattpe@metropolia.fi>
2024-02-06 14:40:01 +0200

    hooks part A done

---
+++ src/components/Example.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, useRef } from 'react';
+
+function Example() {
+    const [count, setCount] = useState<number>(0);
+    const testElement = useRef<HTMLDivElement>(null);
+
+
+    useEffect(() => {
+        // Update div element's color based on count
+        testElement.current?.style.setProperty('color', `rgb(${count * 5}, 0, 0)`);
+        return () => {console.log('component closed')};
+    }, [count]); // Only re-run the effect if count changes
+
+    return (
+        <div ref={testElement}>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+}
+
+export default Example;
