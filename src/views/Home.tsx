53ed281adbe5535abd833aed96df467459ceff49
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-02-09 11:44:28 +0200

    build toimii

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,8 @@
-import {MediaItemWithOwner} from '../types/DBTypes';
 import MediaRow from '../components/MediaRow';
 import {useMedia} from '../hooks/apiHooks';
 
 const Home = () => {
-
-  const mediaArray: MediaItemWithOwner[] = useMedia();
+  const {mediaArray} = useMedia();
 
   return (
     <>
@@ -23,10 +21,7 @@ const Home = () => {
         </thead>
         <tbody>
           {mediaArray.map((item) => (
-            <MediaRow
-              key={item.media_id}
-              item={item}
-            />
+            <MediaRow key={item.media_id} item={item} />
           ))}
         </tbody>
       </table>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~mattpe/react-hooks/',
+  base: '/~ilkkamtk/hybrid/upload/',
 });
