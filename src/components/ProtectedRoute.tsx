3d3e7abb335289aabe21fd2d8697ede4154c9dec
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-02-09 10:10:27 +0200

    context continie

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,9 +4,12 @@ import {useUserContext} from '../hooks/ContextHooks';
 
 const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
   const {user} = useUserContext();
+  const location = useLocation();
 
   if (!user) {
-    return <Navigate to="/" />;
+    console.log('lokaatio', location);
+    // replace and state are used to redirect to origin when page is refreshed
+    return <Navigate to="/" replace state={{from: location}} />;
   }
 
   return children;
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -2,7 +2,7 @@
 import React, {createContext, useState} from 'react';
 import {UserWithNoPassword} from '../types/DBTypes';
 import {useAuthentication, useUser} from '../hooks/apiHooks';
-import {useNavigate} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import {AuthContextType, Credentials} from '../types/LocalTypes';
 
 const UserContext = createContext<AuthContextType | null>(null);
@@ -12,6 +12,7 @@ const UserProvider = ({children}: {children: React.ReactNode}) => {
   const {postLogin} = useAuthentication();
   const {getUserByToken} = useUser();
   const navigate = useNavigate();
+  const location = useLocation();
 
   // login, logout and autologin functions are here instead of components
   const handleLogin = async (credentials: Credentials) => {
@@ -50,8 +51,9 @@ const UserProvider = ({children}: {children: React.ReactNode}) => {
         const userResponse = await getUserByToken(token);
         // set user to state
         setUser(userResponse.user);
-        // navigate to home
-        navigate('/');
+        // when page is refreshed, the user is redirected to origin (see ProtectedRoute.tsx)
+        const origin = location.state.from.pathname || '/';
+        navigate(origin);
       }
     } catch (e) {
       console.log((e as Error).message);
